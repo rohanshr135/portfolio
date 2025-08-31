22173644178b64add8752bd00a514e3f3f8254d2
Rohan <rohan.mech22@iitg.ac.in>
2025-08-31 17:54:17 +0530

    Fix image paths for Vercel deployment

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,5 @@
 const getImagePrefix = () => {
-  return process.env.NODE_ENV === "production"
-    ? "/Next.js-Tailwind-CSS-Portfolio-Template/": "";
+  return "";
 };
 
 export { getImagePrefix };
